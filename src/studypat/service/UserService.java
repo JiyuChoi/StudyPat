26ec36c734ac1dbc2c9a374a7dea8e39b4c473e8
박성민 <vsm0803@gmail.com>
2021-02-22 14:45:35 +0900

    main 내 스크랩 수정

--- src/studypat/service/UserService.java
+++ src/studypat/service/UserService.java
@@ -46,10 +46,6 @@ public class UserService {
 		return userMapper.getUser(id);
 	}
 	
-	public int getUserNo(String id) {
-		return userMapper.getUserNo(id);
-	}
-	
 	public void updateUser(User user, String password, String updatePassword, HttpServletResponse response, RedirectAttributes rttr) throws IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
@@ -154,11 +150,6 @@ public class UserService {
 	public void deleteUser(int userNo) {
 		userMapper.deleteUser(userNo);
 	}
-	public int getUserNo(String id) {
-		
-		return userMapper.getUserNo(id);
-	}
-
 
 }
 
